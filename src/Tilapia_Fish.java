f0f854ff0162716319446a4b17990578d27c5dea
akorgodwin <118388052+akorgodwin@users.noreply.github.com>
2025-04-11 20:28:48 +0100

    Update Tilapia_Fish.java

--- src/Tilapia_Fish.java
+++ src/Tilapia_Fish.java
@@ -1,7 +1,7 @@
 public class Tilapia_Fish extends Aquatic_Animals{
 
-    // Here the  Tilapia_Fish class inherited the Aquatic animal class which has already inherited the animal class
-    // And they all have methods which were in turn printed out by creating an object for each classes
+    // Here, the  Tilapia_Fish class inherited the Aquatic animal class, which has already inherited the animal class
+    // And they all have methods which were in turn printed out by creating an object for each class
 
     public void animal_type(){
 
@@ -13,7 +13,7 @@ public class Tilapia_Fish extends Aquatic_Animals{
         Tilapia_Fish water_animal = new Tilapia_Fish();
         Aquatic_Animals  aquatic = new Aquatic_Animals();
 
-        water_animal.animal_type();
+        water_animal.animal_type();  // the object of the classes created here displays each method
         aquatic.Habitat();
         aquatic.life();
         water_animal.life();
