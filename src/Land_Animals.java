d7eae7e41691f145013fdba899c5a701bfc431db
akorgodwin <118388052+akorgodwin@users.noreply.github.com>
2025-04-11 20:29:42 +0100

    Update Land_Animals.java

--- src/Land_Animals.java
+++ src/Land_Animals.java
@@ -1,7 +1,7 @@
 public class Land_Animals {
      // HIERARCHICAL INHERITANCE
     // Here the land_animal is the superclass inherited by the two subclasses Domestic and Wild
-    // The Domestic extends the Land_animal and the Wild also extends the land_animal
+    // The Domestic extends the Land_animal and the Wild also extends the land_animal as well
 
     void Definition (){
 
