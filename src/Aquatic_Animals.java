5be0568e9e2a23a22075803213e1c5487c3b3b3b
akorgodwin <118388052+akorgodwin@users.noreply.github.com>
2025-04-11 20:25:52 +0100

    Update Aquatic_Animals.java

--- src/Aquatic_Animals.java
+++ src/Aquatic_Animals.java
@@ -2,7 +2,8 @@ public class Aquatic_Animals extends Animals{
 
     // MULTI LEVEL INHERITANCE
     // ANIMAL----AQUATIC_ANIMAL---- TILAPIA_FISH
-    // Here the Aquatic Animal class inherited the Animal class
+    // Here, the Animal class is the super class inherited by the Aquatic Animal class, which was in turn inherited by the Tilapia_Fish class
+
 
     public void Habitat (){
 
